cd94a9f7df194d8967dd956072993cafe089cf9e
Piyush <irock0093@gmail.com>
2020-05-29 23:49:15 +0530

    Making the admin-client functionality, yet to add logic in the new app

--- chat/consumers.py
+++ chat/consumers.py
@@ -88,7 +88,77 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def chat_message_to_client(self, event):
         await self.send(text_data=json.dumps({
-            'room_name': event['room_name'],
+        'room_name': event['room_name'],
             'message': event['message'],
             'message_type': event['message_type'],
         }))
+
+
+
+
+
+
+
+
+
+
+class AdminChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        user = self.scope['user']
+        print('user is', user)
+        if user.is_authenticated and user.is_superuser:
+            print('User is admin')
+            self.accept()
+        else:
+            print('User isnt admin')
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        print('event', event)
+        message = event['message']
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+
+
+
+
+
+
+
+
+
+
+
