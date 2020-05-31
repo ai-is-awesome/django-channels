13006af0b14bd2ab6636a46660d7338ca2e025c9
Piyush <irock0093@gmail.com>
2020-05-31 16:26:21 +0530

    Locked the admin page but database error, inconfigured

--- chat/views.py
+++ chat/views.py
@@ -12,6 +12,14 @@ def room(request, room_name):
 	})
 
 def adminroom(request, room_name):
-	return render(request, 'chat/admin_room.html', {
-		'room_name': room_name
-	})
+	
+	if request.user.is_authenticated and request.user.is_superuser:
+		admin = True
+
+	else:
+		admin = False
+
+
+	context = {'room_name' : room_name, 
+				'admin': admin }
+	return render(request, 'chat/admin_room.html', context)
